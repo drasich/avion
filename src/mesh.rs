5f52258834826c779b4ccefe49d29eefde6fa7d0
chris <chris@indefini.org>
2014-08-17 23:40:46 +0900

    buffer generic trait impl

--- src/mesh.rs
+++ src/mesh.rs
@@ -1,4 +1,5 @@
 extern crate libc;
+use std::collections::{DList,Deque};
 
 use self::libc::{c_int, c_uint, c_void};
 use std::mem;
@@ -8,40 +9,75 @@ pub struct CglBuffer;
 
 #[link(name = "cypher")]
 extern {
-    /*
-    pub fn buffer_request_add(
-        mesh : *mut Mesh,
-        vertex : *const c_void,
-        count : c_int,
-        cb: extern fn(*mut Mesh, i32, *const Buffer)
-        ) -> c_int;
-        */
-
     pub fn cgl_buffer_init(
         vertex : *const c_void,
         count : c_uint
         ) -> *const CglBuffer;
 }
 
+pub struct Buffer<T>
+{
+    pub name: String,
+    pub data : Vec<T>,
+    pub cgl_buffer: Option<*const CglBuffer>,
+}
+
+impl<T> Buffer<T>
+{
+    pub fn new(name: String, data : Vec<T>) -> Buffer<T>
+    {
+        Buffer {
+            name : name,
+            data : data,
+            cgl_buffer : None}
+    }
+}
+
+pub trait BufferSend
+{
+    fn send(&mut self) -> *const CglBuffer;
+}
+
+impl<T> BufferSend for Buffer<T> {
+    fn send(&mut self) -> *const CglBuffer
+    {
+        unsafe {
+            let cgl_buffer = cgl_buffer_init(
+                mem::transmute(self.data.as_ptr()),
+                self.data.len() as c_uint);
+            self.cgl_buffer = Some(cgl_buffer);
+            return cgl_buffer;
+        }
+    }
+}
 
 pub struct Mesh
 {
     pub name : String,
+    //TODO remove buffer
     pub buffer: Option<*const CglBuffer>,
     pub state : i32,
-    pub vertex : Vec<f32>
+    pub vertex : Vec<f32>,
+    pub buffers : DList<Box<BufferSend>>,
 }
 
 impl Mesh
 {
     pub fn new() -> Mesh
     {
-       Mesh {
+       let mut m = Mesh {
            name : String::from_str("mesh_new"),
            buffer : None,
            state : 0,
-           vertex : Vec::from_slice(VERTEX_DATA)
-       }
+           vertex : Vec::from_slice(VERTEX_DATA),
+           buffers : DList::new(),
+       };
+
+       m.buffers.push( box Buffer::new(
+               String::from_str("mybuf"),
+               Vec::from_slice(VERTEX_DATA)));
+
+       return m;
     }
 }
 
@@ -51,9 +87,9 @@ impl resource::ResourceT for Mesh
     {
         if self.state != 11 {
             unsafe {
-                self.buffer = Some( cgl_buffer_init(
-                        mem::transmute(self.vertex.as_ptr()),
-                        self.vertex.len() as c_uint) );
+                for b in self.buffers.mut_iter() {
+                    self.buffer = Some(b.send());
+                }
             }
             self.state = 11;
         }
