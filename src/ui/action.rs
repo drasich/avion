c1a19fa427dd65dc936f554ac51eab934e265224
chris <chris@indefini.org>
2015-02-17 23:11:29 +0900

    actionwip

--- src/ui/action.rs
+++ src/ui/action.rs
@@ -20,15 +20,30 @@ use control::Control;
 #[repr(C)]
 pub struct JkAction;
 
+pub type ButtonCallback = extern fn(
+    data : *const c_void);
+
 #[link(name = "joker")]
 extern {
     fn window_action_new(window : *const Window) -> *const JkAction;
+    fn action_button_new(
+        action : *const JkAction,
+        name : *const c_char,
+        data : *const c_void,
+        button_callback : ButtonCallback);
 }
 
 pub struct Action
 {
     pub name : String,
     pub jk_action : *const JkAction,
+}
+
+pub struct ActionData
+{
+    pub tree : Rc<RefCell<Box<ui::Tree>>>,
+    pub property : Rc<RefCell<Box<ui::Property>>>,
+    pub control : Rc<RefCell<Control>>,
 }
 
 impl Action
@@ -42,9 +57,26 @@ impl Action
             jk_action : unsafe {window_action_new(window)},
         };
 
+        a.add_button("Add empty", add_empty);
+
         a
     }
 
+    fn add_button(&self, name : &str, cb : ButtonCallback)
+    {
+        unsafe {
+            action_button_new(
+                self.jk_action,
+                CString::from_slice(name.as_bytes()).as_ptr(),
+                ptr::null(),
+                cb);
+        }
+    }
+}
+
+extern fn add_empty(data : *const c_void)
+{
+    println!("add empty____________");
 }
 
 
