a5c88f74200edfe50c990c8dd32300e512da7988
Christophe Sadoine <chris@indefini.org>
2016-09-26 19:44:39 +0900

    ground

---
+++ src/model/ground.rs
@@ -0,0 +1,13 @@
+use dormin::factory;
+use dormin::object;
+use dormin::mesh;
+//use mesh_render;
+use dormin::resource;
+use std::sync::{Arc,RwLock};
+
+pub fn create_ground_mesh() -> mesh::Mesh
+{
+    let mut m = mesh::Mesh::new();
+    m
+}
+
